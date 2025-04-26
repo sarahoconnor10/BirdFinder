e8527f6aa7ab661a15b49e9161d8b4ca43d4d82d
Sarah O Connor <sarahaoconnor10@gmail.com>
2025-04-26 18:10:22 +0100

    Implemented base profile page

--- BirdFinder/src/app/pages/profile/profile.page.ts
+++ BirdFinder/src/app/pages/profile/profile.page.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonBackButton  } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonButton, IonBackButton ]
+  imports: [CommonModule, FormsModule, IonicModule]
 })
 export class ProfilePage implements OnInit {
+  userName = 'Guest User';
+  darkModeEnabled = false;
+  notificationsEnabled = true;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleDarkMode() {
+    document.body.classList.toggle('dark', this.darkModeEnabled);
   }
 
 }
