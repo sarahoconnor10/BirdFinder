c0874a7dc80965e129d00de9a65c8decc22e4b08
Sarah O Connor <sarahaoconnor10@gmail.com>
2025-04-26 14:07:51 +0100

    camera page basic elements and styling

--- BirdFinder/src/app/pages/camera/camera.page.ts
+++ BirdFinder/src/app/pages/camera/camera.page.ts
@@ -1,20 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonBackButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonBackButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { closeOutline, camera, imagesOutline, radioButtonOn, flashOutline } from 'ionicons/icons';
+import { RouterLinkWithHref } from '@angular/router';
+
+addIcons({
+  "close-outline": closeOutline,
+  "camera": camera,
+  "images-outline": imagesOutline,
+  "radio-button-on": radioButtonOn,
+  "flash-outline": flashOutline
+})
 
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.page.html',
   styleUrls: ['./camera.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonButton, IonBackButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonButton, IonBackButton, IonIcon, RouterLinkWithHref]
 })
 export class CameraPage implements OnInit {
+  @ViewChild('videoElement', { static: false }) videoElement!: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  captureImage() {
+    console.log("Image captured");
+
+  }
+
+
+  openGallery() {
+    console.log("Gallery opened");
+
+  }
+
+  toggleFlash() {
+    console.log("Flash toggled");
   }
 
 }
