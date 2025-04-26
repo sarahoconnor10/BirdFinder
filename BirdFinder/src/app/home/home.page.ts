d1091d1c3187421a82dfb661685b2dad8ad5602a
Sarah O Connor <sarahaoconnor10@gmail.com>
2025-04-26 17:24:42 +0100

    Added recently spotted birds to homepage

--- BirdFinder/src/app/home/home.page.ts
+++ BirdFinder/src/app/home/home.page.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonBackButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonBackButton, IonList, IonItem, IonThumbnail, IonLabel } from '@ionic/angular/standalone';
 import { RouterLinkWithHref } from '@angular/router';
+import { BirdCollectionService } from '../services/bird-collection.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, RouterLinkWithHref, IonBackButton],
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, RouterLinkWithHref, IonBackButton, IonList, IonItem, IonThumbnail, IonLabel],
 })
 export class HomePage {
-  constructor() {}
+  recentBirds: any[] = [];
+
+  constructor(private birdCollectionService: BirdCollectionService) { }
+
+  ionViewWillEnter() {
+    this.loadRecentBirds();
+  }
+
+  loadRecentBirds() {
+    const allBirds = this.birdCollectionService.getSavedBirds();
+    this.recentBirds = allBirds.reverse().slice(0, 5);
+  }
+
 }
