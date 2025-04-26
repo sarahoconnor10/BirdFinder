97100759c5811303751187d187f316f03029ac37
Sarah O Connor <sarahaoconnor10@gmail.com>
2025-04-26 17:05:19 +0100

    Collection page functionality to display saved birds

--- BirdFinder/src/app/pages/collection/collection.page.ts
+++ BirdFinder/src/app/pages/collection/collection.page.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonBackButton  } from '@ionic/angular/standalone';
-
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonBackButton, IonList, IonThumbnail, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { BirdCollectionService } from 'src/app/services/bird-collection.service';
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.page.html',
   styleUrls: ['./collection.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonButton, IonBackButton ]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonButton, IonBackButton, IonList, IonThumbnail, IonItem, IonLabel]
 })
 export class CollectionPage implements OnInit {
+  birds: any[] = [];
 
-  constructor() { }
+  constructor(private birdCollectionService: BirdCollectionService) { }
+
+  ionViewWillEnter() {
+    this.loadBirds();
+  }
 
   ngOnInit() {
+  }
+
+  loadBirds() {
+    this.birds = this.birdCollectionService.getSavedBirds();
+    console.log('Loaded birds:', this.birds);
   }
 
 }
